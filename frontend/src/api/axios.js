6535cd665d3338288dea2248e65636b897e28462
kajolsinghme <kajolsingh985@gmail.com>
2026-02-15 17:14:30 +0530

    updated eslint config and fixed issue in env variable

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
 
-console.log(process.env.REACT_APP_BASE_URL)
 export const api = axios.create({
-  baseURL: `${process.env.REACT_APP_BASE_URL ?? `http://localhost:4000`}/api/v1`,
+  baseURL: `${import.meta.env.VITE_API_URL ?? `http://localhost:4000`}/api/v1`,
 });
