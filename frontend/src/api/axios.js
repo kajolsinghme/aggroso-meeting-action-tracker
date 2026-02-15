48d124928e78abbfad001067b4f449a21b060022
kajolsinghme <kajolsingh985@gmail.com>
2026-02-15 17:09:19 +0530

    added: a test console log

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+
+console.log(process.env.REACT_APP_BASE_URL)
 export const api = axios.create({
   baseURL: `${process.env.REACT_APP_BASE_URL ?? `http://localhost:4000`}/api/v1`,
 });
